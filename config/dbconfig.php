ef02f4e2675baaa6e941a7619957ec380b5466b6
SGAIntegrador <developerstrategico2@gmail.com>
2024-05-02 11:04:02 -0500

    Credenciales Actualizadas

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,15 +1,19 @@
 <?php
 
+// $DB_host = "localhost";
+// //$DB_host = "52.15.158.65:3306";
+// $DB_user = "root";
+// $DB_pass = "";
+// $DB_name = "grupoasi_cotizautos";
 $DB_host = "localhost";
-//$DB_host = "52.15.158.65:3306";
-$DB_user = "root";
-$DB_pass = "";
+$DB_user = "grupoasi_cotizautos";
+$DB_pass = "M1graci0n123";
 $DB_name = "grupoasi_cotizautos";
 
 
 try
 {
-	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
 	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,12 +5,12 @@
 class Conexion{
 
 	static public function conectar(){
+		// $link = new PDO("mysql:host=localhost;dbname=grupoasi_cotizautos",
+		//  	            "root",
+		//  	            "");
 		$link = new PDO("mysql:host=localhost;dbname=grupoasi_cotizautos",
-		 	            "root",
-		 	            "");
-		//$link = new PDO("mysql:host=52.15.158.65:3306;dbname=grupoasi_cotizautos",
-		//	            "grupoasi_cotizautos",
-		//	            "M1graci0n123");
+			            "grupoasi_cotizautos",
+			            "M1graci0n123");
 		$link->exec("set names utf8");
 
 		return $link;
